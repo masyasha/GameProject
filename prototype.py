99b6b39c4e4cb168db90f77516eb139ddb38971f
Seva <richello3027@gmail.com>
2016-03-30 14:56:14 +0300

    prototype

---
+++ prototype.py
@@ -0,0 +1,52 @@
+import pygame as py
+import random
+
+
+class Block:
+    def __init__(self, w=40, h=40):
+        self.w = w
+        self.h = h
+        self.x = self.y = 0
+        self.block_colour = (254,89,90)
+        self.level = ["---------------",
+                      "-",
+                      "-",
+                      "-",
+                      "---------------",
+                      "              -",
+                      "              -",
+                      "              -",
+                      "---------------"]
+
+
+class Base:
+    def __init__(self):
+        self.main()
+    def main(self, Play=True):
+        py.init()
+        self.window = py.display.set_mode((800, 640))
+        py.display.set_caption('Psyho Masya')
+        bgColour = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+        self.window.fill(bgColour)
+        self.draw()
+        while Play:
+            for event in py.event.get():
+                if event.type == py.QUIT:
+                    Play = False
+            py.display.update()
+    """ FUNCTIONS """
+    def draw(self):
+        for row in b.level:
+            for symbol in row:
+                if symbol == '-':
+                    self.block = py.Surface((b.w, b.h))
+                    self.block.fill(b.block_colour)
+                    self.window.blit(self.block, (b.x,b.y))
+                b.x += b.w
+            b.y += b.h
+            b.x = 0
+
+
+b = Block()
+base = Base()
+py.quit()
