cd2a5de1864e2372081cf41884f060080e6650d2
Seva <richello3027@gmail.com>
2016-04-01 22:09:29 +0300

    Blocks File

---
+++ Blocks.py
@@ -0,0 +1,14 @@
+from pygame import *
+import pygame as py
+import random as ran
+
+PLATFORM_W = 32
+PLATFORM_H = 32
+PLATFORM_COLOUR = (ran.randint(0,255), ran.randint(0,255), ran.randint(0,255))
+
+class Blocks(sprite.Sprite):
+    def __init__(self,x,y):
+        sprite.Sprite.__init__(self)
+        self.image = Surface((PLATFORM_W, PLATFORM_H))
+        self.image.fill(PLATFORM_COLOUR)
+        self.rect = Rect(x, y, PLATFORM_W, PLATFORM_H)
