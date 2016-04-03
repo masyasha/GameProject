a83578ced34ee46e6b08c74f3285e40d5f62b904
Seva <richello3027@gmail.com>
2016-04-03 21:11:35 +0300

    ENEMY

---
+++ PROTOTYPE/Enemy.py
@@ -0,0 +1,21 @@
+from pygame import *
+import pygame as py
+import time
+
+ENEMY_W = 25
+ENEMY_H = 35
+ENEMY_COLOUR = (255,0,0)
+ENEMY_SPEED = 0.1
+
+
+class Enemy(sprite.Sprite):
+    def __init__(self):
+        sprite.Sprite.__init__(self)
+        x, y = 1504, 125
+        self.image = Surface((ENEMY_W,ENEMY_H))
+        self.image.fill(ENEMY_COLOUR)
+        self.rect = Rect(x, y, ENEMY_W, ENEMY_H)
+    def update(self, start):
+        if start:
+            self.image.fill(ENEMY_COLOUR)
+            self.rect.x += ENEMY_SPEED
