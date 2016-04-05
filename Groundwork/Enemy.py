b6ccfba32af3521ec2921171640e3deae358ef57
Seva <richello3027@gmail.com>
2016-04-05 07:48:55 +0300

    Enemy

--- Groundwork/Enemy.py
+++ Groundwork/Enemy.py
@@ -4,7 +4,7 @@ import pygame as py
 ENEMY_W = 25
 ENEMY_H = 35
 ENEMY_COLOUR = (255,0,0)
-ENEMY_SPEED = 1
+ENEMY_SPEED = 0.1
 
 
 class Enemy(sprite.Sprite):
@@ -14,11 +14,13 @@ class Enemy(sprite.Sprite):
         self.image = Surface((ENEMY_W,ENEMY_H))
         self.image.fill(ENEMY_COLOUR)
         self.rect = Rect(x, y, ENEMY_W, ENEMY_H)
-    def update(self, start, screen, alive):
+    def update(self, pl_x, pl_y, start, alive):
         if start and alive:
-            self.image.fill(ENEMY_COLOUR)
-            print self.rect.x
-            while self.rect.x != 950:
-                self.rect.x -= ENEMY_SPEED
-            screen.blit(self.image, (self.rect.x, self.rect.y))
-
+            self.rect.x -= ENEMY_SPEED
+            self.collision(self.rect.x, self.rect.y, pl_x, pl_y)
+    def collision(self, BLOCK_x, BLOCK_y, MARIO_x, MARIO_y):
+        check = (((MARIO_x == BLOCK_x) and (MARIO_y == BLOCK_y + 5)) or
+                 ((MARIO_x + 20 == BLOCK_x) and (MARIO_y == BLOCK_y + 5)))
+        print BLOCK_x, BLOCK_y, MARIO_x, MARIO_y
+        if check:
+            print "WORKED!!!"
