7210b753496abe4fafa4de742b7b08cddd059a9f
Seva <richello3027@gmail.com>
2016-04-06 15:01:46 +0300

    Enemy

--- Groundwork/Enemy.py
+++ Groundwork/Enemy.py
@@ -4,13 +4,14 @@ import pygame as py
 ENEMY_W = 25
 ENEMY_H = 35
 ENEMY_COLOUR = (255,0,0)
-ENEMY_SPEED = 0.1
+ENEMY_SPEED = 0.01
 
 
 class Enemy(sprite.Sprite):
     def __init__(self):
         sprite.Sprite.__init__(self)
         x, y = 1504, 125
+        self.pl_alive = True
         self.image = Surface((ENEMY_W,ENEMY_H))
         self.image.fill(ENEMY_COLOUR)
         self.rect = Rect(x, y, ENEMY_W, ENEMY_H)
@@ -19,8 +20,10 @@ class Enemy(sprite.Sprite):
             self.rect.x -= ENEMY_SPEED
             self.collision(self.rect.x, self.rect.y, pl_x, pl_y)
     def collision(self, BLOCK_x, BLOCK_y, MARIO_x, MARIO_y):
-        check = (((MARIO_x == BLOCK_x) and (MARIO_y == BLOCK_y + 5)) or
-                 ((MARIO_x + 20 == BLOCK_x) and (MARIO_y == BLOCK_y + 5)))
-        print BLOCK_x, BLOCK_y, MARIO_x, MARIO_y
+        check = (((BLOCK_x <= MARIO_x <= BLOCK_x + 25)
+                and (BLOCK_x <= MARIO_x + 20 <= BLOCK_x + 25))
+                and ((BLOCK_y <= MARIO_y <= BLOCK_y + 35)
+                and (BLOCK_y <= MARIO_y + 30 <= BLOCK_y + 35)))
+
         if check:
-            print "WORKED!!!"
+            self.pl_alive = False
